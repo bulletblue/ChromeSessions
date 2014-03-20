2530e50271d36a31f1085d5410f1d4e2764c00d9
Mikhail Sapozhnikov <bulletblue@gmail.com>
2014-03-20 02:02:02 -0400

    add button, create js class

--- popup.js
+++ popup.js
@@ -1,17 +1,36 @@
-function getSession()  {
-    var newP = document.createElement("p");
-    var someText = document.createTextNode("Hello world!");
+var currentDiv = document.getElementById("somediv");
 
-    newP.appendChild(someText);
-    var currentdiv = document.getElementById("somediv");
-    document.body.insertBefore(newP, currentdiv);
+function doc_insert(el) {
+    document.body.insertBefore(el, currentDiv);
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  getSession();
-});
+var Sessions = {
 
-/*chrome.tabs.query({'currentWindow': true}, function(tabs) {
-    var count = document.getElementById('tabCount');
-    count.innerHTML = "5";
-});*/
+    startup: function() {
+        var button = document.createElement("BUTTON");
+        var buttonText = document.createTextNode("Click Me");
+        button.appendChild(buttonText);
+        doc_insert(button);
+
+        //retrieve all sessions
+    },
+
+    getSession: function()  {
+        chrome.tabs.query({'currentWindow': true}, function(tabs) {
+            var newP = document.createElement("p");
+            var someText = document.createTextNode(tabs[1].url);
+            newP.appendChild(someText);
+            doc_insert(newP);
+            //document.body.insertBefore(newP, currentDiv);
+        });
+    },
+
+    scroll: function() {
+    }
+};
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  Sessions.startup();
+});

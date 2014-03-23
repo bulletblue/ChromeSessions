31939283aac0ac6d476713166fa52df8256441fb
Mikhail Sapozhnikov <bulletblue@gmail.com>
2014-03-23 00:14:59 -0400

    Consolidate code, session keys appear on start up.

--- popup.js
+++ popup.js
@@ -10,11 +10,8 @@ var Sessions = {
     startup: function() {
         var me = Sessions;
 
-        /*var getButton = document.createElement("BUTTON");
-        getButton.onclick = me.getSession;
-        var getButtonText = document.createTextNode("Get Session");
-        getButton.appendChild(getButtonText);
-        doc_insert(getButton);*/
+        //Display saved sessions on start
+        me.getSessions();
 
         var setButton = document.createElement("BUTTON");
         setButton.onclick = me.saveSession;
@@ -23,21 +20,16 @@ var Sessions = {
         doc_insert(setButton);
     },
 
-    getSession: function()  {
-        //OPEN THE SESSION HERE!
-        //chrome.windows.create({url: "http://yahoo.com", focused: true});
-
+    getSessions: function()  {
         chrome.storage.sync.get(null, function(items) {
-            var allLinks = "";
-            for (var i = 0; i < items.urls.length; i++) {
-                allLinks += items.urls[i] + ",";
+            console.log(items);
+            for (var i = 0; i < items.length; i++) {
+                var newP = document.createElement("p");
+                var someText = document.createTextNode(items[i].sessionKey);
+                newP.appendChild(someText);
+                doc_insert(newP);
+                document.body.insertBefore(newP, currentDiv);
             }
-
-            var newP = document.createElement("p");
-            var someText = document.createTextNode(allLinks);
-            newP.appendChild(someText);
-            doc_insert(newP);
-            document.body.insertBefore(newP, currentDiv);
         });
     },
 
@@ -52,9 +44,9 @@ var Sessions = {
 
             sessionKey = Date.now();
             
-            chrome.storage.sync.set({sessionKey: urls}, function() {
+            chrome.storage.sync.set({"urls": urls, "sessionKey": sessionKey}, function() {
                 var getLink = document.createElement("p");
-                getLink.onclick = me.openSession(sessionKey);
+                getLink.onclick = me.openSession;
                 var getLinkText = document.createTextNode(sessionKey);
                 getLink.appendChild(getLinkText);
                 doc_insert(getLink);
@@ -62,23 +54,20 @@ var Sessions = {
         });
     },
 
-    openSession: function(sessionKey) {
-        //OPEN THE SESSION HERE!
-        //chrome.windows.create({url: "http://yahoo.com", focused: true});
+    openSession: function() {
+        chrome.storage.sync.get(null, function(items) {
+            //chrome.windows.create({url: items.urls, focused: true});
 
-        chrome.storage.sync.get(sessionKey.toString(), function(items) {
-            chrome.windows.create({url: items.urls, focused: true});
-
-            /*var allLinks = [];
+            var allLinks = '';
             for (var i = 0; i < items.urls.length; i++) {
-                allLinks = items.urls[i];
-            }*/
+                allLinks += items.urls[i] + ",";
+            }
 
-            /*var newP = document.createElement("p");
+            var newP = document.createElement("p");
             var someText = document.createTextNode(allLinks);
             newP.appendChild(someText);
             doc_insert(newP);
-            document.body.insertBefore(newP, currentDiv);*/
+            document.body.insertBefore(newP, currentDiv);
         });
     }
 };
@@ -87,11 +76,3 @@ var Sessions = {
 document.addEventListener('DOMContentLoaded', function () {
   Sessions.startup();
 });
-
-
-
-/*var newP = document.createElement("p");
-var someText = document.createTextNode(allTabs);
-newP.appendChild(someText);
-doc_insert(newP);
-document.body.insertBefore(newP, currentDiv);*/

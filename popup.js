c871c0ede08748bfec1b352091c5810f6f859e28
Mikhail Sapozhnikov <bulletblue@gmail.com>
2014-06-06 18:18:05 -0400

    Fix multiplying event listener in input field. Refactor and simplify JS.

--- popup.js
+++ popup.js
@@ -1,22 +1,24 @@
-var saveLock = 0;
-
-function keyExists(key, sessions) {
-    for (var i = 0; i < sessions.length; i++)
+function keyExists(key, sessionsList) {
+    for (var i = 0; i < sessionsList.length; i++)
     {
-        if (key === sessions[i]) return true;
+        if (key === sessionsList[i]) {
+            return true;
+        }
     }
     return false;
 }
 
 function hideSaveElements() {
     document.getElementById("msg_alert").style.visibility = "hidden";
+    document.getElementById("msg_alert").innerHTML = "Enter a session name";
     document.getElementById("input_session").style.visibility = "hidden";
     document.getElementById("btn_cancelSave").style.visibility = "hidden"; 
 }
 
-function showSaveElements() {  
+function showSaveElements() {
     document.getElementById("msg_alert").style.visibility = "visible";
     document.getElementById("input_session").style.visibility = "visible";
+    document.getElementById("input_session").focus();
     document.getElementById("btn_cancelSave").style.visibility = "visible"; 
 }
 
@@ -26,7 +28,7 @@ var sessions = {
         this.getSessions();
         
         var saveButton = document.getElementById("btn_save");
-        saveButton.onclick = this.saveSession;
+        saveButton.onclick = showSaveElements;
 
         var deleteButton = document.getElementById("btn_deleteAll");
         deleteButton.onclick = this.deleteAll;
@@ -34,132 +36,58 @@ var sessions = {
         var cancelSaveButton = document.getElementById("btn_cancelSave");
         cancelSaveButton.onclick = this.cancelSave;
 
+        var inputSession = document.getElementById("input_session");
+        inputSession.addEventListener("keydown", sessions.saveSession2);
         hideSaveElements();
     },
 
     getSessions: function() {
-        //var that = sessions;
-        chrome.storage.sync.get(null, function(items) {
-            for (var item in items) {
-
-                sessions.setInTable(item, items[item]);
-
-                // var session = document.createElement("p");
-                // session.setAttribute("class", "session_cell ellipses");
-                // session.appendChild(document.createTextNode(item));
-
-                // var iconRemove = document.createElement("i");
-                // iconRemove.setAttribute("class", "fa fa-minus");
-
-                // var removeP = document.createElement("p");
-                // removeP.setAttribute("class", "remove_session");
-                // removeP.appendChild(iconRemove);
-
-                // var table = document.getElementById("sessions_table");
-                // var row = table.insertRow(0);
-
-                // that.openSessionEV(session, items[item]);
-                // that.removeSessionEV(removeP, item);
-                
-                // var cellSession = row.insertCell(0);
-                // cellSession.width = "100%";
-                // cellSession.appendChild(session);
-
-                // var cellRemove = row.insertCell(1);
-                // cellRemove.appendChild(removeP);
-            }
-        });
+        chrome.storage.sync.get(null, function(items) { for (var item in items) { sessions.setInTable(item, items[item]); }});
     },
 
-    saveSession: function() {
-        if (saveLock === 0) {
-            var that = sessions;
-            saveLock = 1;
-            var sessionKey = "";
+    saveSession2: function(e) {
+        if (e.keyIdentifier === "Enter") {
+            chrome.storage.sync.get(null, function(items) {
 
-            showSaveElements();
+                var sessionsList = [];
+                for (item in items) { 
+                    sessionsList.push(item); 
+                }
 
-            var msgAlert = document.getElementById("msg_alert");
-            var sessionInputField = document.getElementById("input_session");
-            var cancelSaveButton = document.getElementById("btn_cancelSave");
-            sessionInputField.focus();
-
-            sessionInputField.addEventListener("keydown", function func(e) {
-                console.log(e.keyIdentifier);
-                chrome.storage.sync.get(null, function(items) {
-
-                    var sessionsList = [];
-                    for (item in items) {
-                        sessionsList.push(item);
-                    }
-
-                    if (e.keyIdentifier === "Enter") {
-
-                        if (sessionInputField.value === "") {
-                            msgAlert.innerHTML = "Enter a session name";
-                            msgAlert.style.visibility = "visible";
+                var msgAlert = document.getElementById("msg_alert");
+                var sessionInputField = document.getElementById("input_session");
+                var cancelSaveButton = document.getElementById("btn_cancelSave");
+                
+                if (sessionInputField.value === "") {
+                    msgAlert.innerHTML = "Enter a session name";
+                    msgAlert.style.visibility = "visible";
+                }
+                else if (keyExists(sessionInputField.value, sessionsList)) {
+                    msgAlert.innerHTML = "Session already exists!";
+                    msgAlert.style.visibility = "visible";
+                }
+                else {
+                    chrome.tabs.query({"currentWindow": true}, function(tabs) {
+                        var urls = [];
+                        for (var i = 0; i < tabs.length; i++) {
+                            urls[i] = tabs[i].url;
                         }
-                        else if (keyExists(sessionInputField.value, sessionsList)) { 
-                            msgAlert.innerHTML = "Session already exists!";
-                            msgAlert.style.visibility = "visible";
-                        }
-                        else {
-                            sessionKey = sessionInputField.value;
+
+                        var sessionObj = {};
+                        sessionObj[sessionInputField.value] = urls;
+
+                        chrome.storage.sync.set(sessionObj, function() {
+                            sessions.setInTable(sessionInputField.value, urls);
                             sessionInputField.value = "";
                             hideSaveElements();
-                        
-                            chrome.tabs.query({"currentWindow": true}, function(tabs) {
-                                var urls = [];
-                                for (var i = 0; i < tabs.length; i++) {
-                                    urls[i] = tabs[i].url;
-                                }
-
-                                var sessionObj = {};
-                                sessionObj[sessionKey] = urls;
-
-                                chrome.storage.sync.set(sessionObj, function() {
-
-                                    sessions.setInTable(sessionKey, urls);
-
-                                    // var newSession = document.createElement("p");
-                                    // newSession.setAttribute("class", "session_cell");
-                                    // newSession.appendChild(document.createTextNode(sessionKey));
-
-                                    // var iconRemove = document.createElement("i");
-                                    // iconRemove.setAttribute("class", "fa fa-minus");
-
-                                    // var removeP = document.createElement("p");
-                                    // removeP.setAttribute("class", "remove_session");
-                                    // removeP.appendChild(iconRemove);
-
-                                    // var table = document.getElementById("sessions_table");
-                                    // var row = table.insertRow(0);
-
-                                    // that.openSessionEV(newSession, urls);
-                                    // that.removeSessionEV(iconRemove, sessionKey);
-
-                                    // var cellSession = row.insertCell(0);
-                                    // cellSession.width = "100%";
-                                    // cellSession.appendChild(newSession);
-
-                                    // var cellRemove = row.insertCell(1);
-                                    // cellRemove.appendChild(removeP);
-                                });
-                            });
-                            sessionInputField.removeEventListener("keydown", func);
-                            saveLock = 0;
-                        }
-                    }   
-                    else
-                    {
-                        msgAlert.style.visibility = "hidden";
-                    }
-                });
+                        });
+                    });
+                }
             });
-        }
-        else {
-            //document.getElementById("msg_alert").style.visibility = "hidden";
-            var input = document.getElementById("input_session").focus();
+        }   
+        else
+        {
+           document.getElementById("msg_alert").style.visibility = "hidden";
         }
     },
 
@@ -231,7 +159,7 @@ var sessions = {
     cancelSave: function() {
         saveLock = 0;
         document.getElementById('msg_alert').innerHTML = "Enter a session name";
-        document.getElementById("input_session").removeEventListener("keydown", "func");
+        //document.getElementById("input_session").removeEventListener("keydown", "func");
         document.getElementById('input_session').value = "";
         hideSaveElements();
     },
